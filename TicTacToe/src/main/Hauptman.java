ce255d1049bbee2d4b6c00477cc41410ac36c562
Johannes <CommanderFive@gmx.net>
2020-03-15 21:01:17 +0100

    ORIGINAL JZ MIT OHNE SPIELER

--- TicTacToe/src/main/Hauptman.java
+++ TicTacToe/src/main/Hauptman.java
@@ -7,9 +7,13 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Scanner;
+import java.util.Vector;
+
 
 public class Hauptman {
 	public char[] spielfeld = new char[9];
+	public int spielzug;
+	public int rofl = 1;
 
 	public static void main(String[] args) throws IOException {
 		Hauptman mainklasse = new Hauptman();
@@ -24,25 +28,25 @@ public class Hauptman {
 		System.out.println("GEBE NUN EINE ZAHL EIN (0-8)");
 		while (true) {
 			Scanner scanner1 = new Scanner(System.in);
-			int eingabe = scanner1.nextInt();
-			if(eingabeTester(eingabe) == false) {
-				System.out.println("DIE ZAHL GEHT NICHT SRY...VERSUCH ES DOCH NOCHMAL!");
-				continue;
-			}			
-			spielfeld[eingabe] = 'X';						
-			if(endeVonAllem() == true) {
-				break;
-				
-			}
-			else {
-				
-				spielfeld[call911(spielfeldToString())] = 'O';
+				Spielverlauf(rofl);
+				System.out.println("Vorher: ");
 				writeSheet();
+				spielfeld[call911(spielfeldToString())] = 'O';
+				System.out.println("Nacher: ");
+				writeSheet();
+				rofl = rofl +1;
 			
-			}
+				if (hasWon(spielfeldToString(), 'X') == true) {
+					System.out.println("Du hast gewonnen");
+					break;
+					
+				}
+				else if (hasWon(spielfeldToString(), 'O') == true) {
+					System.out.println("Oh man alter du hast gegen einen Algo verloren. Peinlich");
+					break;
+				}
 			
 		}
-		System.out.println("UND VORBEI!");
 	}
 	public String spielfeldToString() {
 		String reuckgabe = new String();
@@ -96,6 +100,65 @@ public class Hauptman {
 		}
 	}
 	
+	public void Spielverlauf(int zugstand) {
+		switch (zugstand) {
+		case 1:
+			spielfeld[6]='X';
+			break;
+		case 2:
+			spielfeld[2] = 'X';
+			break;
+		case 3:
+			if(einzelnerStellenTester(0, 'O') == false) {
+				spielfeld[0] = 'X';
+				spielzug = 0;
+				break;
+			}
+			else if (einzelnerStellenTester(8, 'O') == false){
+				spielfeld[8] = 'X';
+				spielzug = 1;
+				break;
+			}
+		case 4:
+			if(spielzug == 0 && einzelnerStellenTester(1, 'O')== false) {
+				spielfeld[1] = 'X';
+				break;
+			}
+			else if (spielzug == 0 && einzelnerStellenTester(3, 'O')== false) {
+				spielfeld[3] = 'X';
+				break;
+			}
+			if (spielzug == 1 && einzelnerStellenTester(7, 'O')== false) {
+				spielfeld[7] = 'X';
+				break;
+			}
+			else if (spielzug == 1 && einzelnerStellenTester(5, 'O')== false) {
+				spielfeld[5] = 'X';
+				break;
+			}
+		}
+		
+		
+		
+	}
+	public Vector<Integer> stellentester(char Xor0) {
+		Vector<Integer> belegteStellen = null;
+		for (int i = 0 ; i < spielfeld.length;i++) {
+			if(spielfeld[i] == Xor0) {
+				belegteStellen.add(i);
+			}
+		}
+		return belegteStellen;
+	}
+	public boolean einzelnerStellenTester(int stelle, char Xor0) {
+		if(spielfeld[stelle] == Xor0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
 	private static int call911(String currentGame) throws IOException {
 		System.out.println("SENDEN " + currentGame);
         int result = -1;
@@ -114,4 +177,31 @@ public class Hauptman {
         System.out.println("EMPFANGEN " + result);
         return result;
     }
+	private boolean hasWon(String currentGame, char x) {
+	    ROW:
+	    for (int row = 0; row < 3; ++row) {
+	      for (int column = 0; column < 3; ++column) {
+	        if (currentGame.charAt(column * 3 + row) != x) {
+	          continue ROW;
+	        }
+	      }
+	      return true;
+	    }
+	    COLUMN:
+	    for (int column = 0; column < 3; ++column) {
+	      for (int row = 0; row < 3; ++row) {
+	        if (currentGame.charAt(column * 3 + row) != x) {
+	          continue COLUMN;
+	        }
+	      }
+	      return true;
+	    }
+	    if (currentGame.charAt(0) == x && currentGame.charAt(4) == x && currentGame.charAt(8) == x) {
+	      return true;
+	    }
+	    if (currentGame.charAt(2) == x && currentGame.charAt(4) == x && currentGame.charAt(6) == x) {
+	      return true;
+	    }
+	    return false;
+	  }
 }
